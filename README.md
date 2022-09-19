3bb8410de95e728a0cc3de0382d23499178865d9
rhayann1337 <rhayannbianco@gmail.com>
2022-09-18 21:08:02 -0300

    update readme

---
+++ README.md
@@ -0,0 +1,41 @@
+&nbsp;
+
+<p align="center">
+  <a href="#techs">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#project">Project</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#how-use">How to run</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+</p>
+&nbsp;&nbsp;&nbsp;
+
+<div id="techs"></div>
+
+## ✨ Technologies
+
+This project was developed with the following technologies:
+
+- [PHP](https://www.php.net/)
+- [CodeIgniter](https://codeigniter.com/)
+- [MySQL](https://www.mysql.com/)
+- [jQuery](https://jquery.com/)
+- [DomPDF](https://github.com/dompdf/dompdf)
+</div>
+
+<div id="project"></div>
+
+## 💻 Project
+
+This is an application focused on mechanic shop management, being possible to register customers, control application users, control stock parts, services performed and report control of all areas.
+
+&nbsp;&nbsp;&nbsp;
+
+<div id="how-use"></div>
+
+## 🚀 How to run
+
+- Clone the repository
+- You must have installed PHP and xampp
+- You can import a database on phpMyAdmin, the name file is `autokratchei_v1` (It is recommended that you create a new user, the other users have been encrypted in this database)
+- Run the application with xampp
+- Enjoy the application
+
+---
--- application/views/aboutUs/index.php
+++ application/views/aboutUs/index.php
@@ -1,133 +1,139 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  margin: 0;
-}
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+        margin: 0;
+    }
 
-html {
-  box-sizing: border-box;
-  width: 100%;
-  height: 100%;
-  background-color: #393a3e;
-          background-image: linear-gradient(180deg, #181f36 10%, #224abe 100%);
-          background-size: cover;
-          color: white;
-}
+    html {
+        box-sizing: border-box;
+        width: 100%;
+        height: 100%;
+        background-color: #393a3e;
+        background-image: linear-gradient(180deg, #181f36 10%, #224abe 100%);
+        background-size: cover;
+        color: white;
+    }
 
-*, *:before, *:after {
-  box-sizing: inherit;
-}
+    *,
+    *:before,
+    *:after {
+        box-sizing: inherit;
+    }
 
-.column {
-  float: left;
-  width: 33.3%;
-  margin-bottom: 16px;
-  padding: 0 8px;
-}
+    .column {
+        float: left;
+        width: 33.3%;
+        margin-bottom: 16px;
+        padding: 0 8px;
+    }
 
-.card {
-    border-radius: 8px;
-    background-color: rgba(255, 255, 255, 0.2);
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  margin: 8px;
-}
+    .card {
+        border-radius: 8px;
+        background-color: rgba(255, 255, 255, 0.2);
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        margin: 8px;
+    }
 
-.about-section {
-  padding: 50px;
-  text-align: center;
-          color: white;
-}
+    .about-section {
+        padding: 50px;
+        text-align: center;
+        color: white;
+    }
 
-.container {
-  padding: 0 16px;
-}
+    .container {
+        padding: 0 16px;
+    }
 
-.container::after, .row::after {
-  content: "";
-  clear: both;
-  display: table;
-}
+    .container::after,
+    .row::after {
+        content: "";
+        clear: both;
+        display: table;
+    }
 
-.title {
-  color: grey;
-}
+    .title {
+        color: grey;
+    }
 
-.button {
-  border: none;
-  outline: 0;
-  display: inline-block;
-  padding: 8px;
-  color: white;
-  background-color: #000;
-  text-align: center;
-  cursor: pointer;
-  width: 100%;
-}
+    .button {
+        border: none;
+        outline: 0;
+        display: inline-block;
+        padding: 8px;
+        color: white;
+        background-color: #000;
+        text-align: center;
+        cursor: pointer;
+        width: 100%;
+    }
 
-.button:hover {
-  background-color: #555;
-}
+    .button:hover {
+        background-color: #555;
+    }
 
-@media screen and (max-width: 650px) {
-  .column {
-    width: 100%;
-    display: block;
-  }
-}
-</style>
+    @media screen and (max-width: 650px) {
+        .column {
+            width: 100%;
+            display: block;
+        }
+    }
+    </style>
 </head>
+
 <body>
 
-<div class="about-section">
-  <h1>About Us</h1>
-  <p>Equipe de desenvolvimento do projeto Auto Kratchei.</p>
-</div>
+    <div class="about-section">
+        <h1>About Us</h1>
+        <p>Equipe de desenvolvimento do projeto Auto Mac.</p>
+    </div>
 
-<h2 style="text-align:center">Our Team</h2>
-<div class="row">
-  <div class="column">
-    <div class="card">
-      <img src="/w3images/team1.jpg" alt="Jane" style="width:100%">
-      <div class="container">
-        <h2>Jane Doe</h2>
-        <p class="title">CEO & Founder</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>jane@example.com</p>
-        <p><button class="button">Contact</button></p>
-      </div>
-    </div>
-  </div>
+    <h2 style="text-align:center">Our Team</h2>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="/w3images/team1.jpg" alt="Jane" style="width:100%">
+                <div class="container">
+                    <h2>Jane Doe</h2>
+                    <p class="title">CEO & Founder</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>jane@example.com</p>
+                    <p><button class="button">Contact</button></p>
+                </div>
+            </div>
+        </div>
 
-  <div class="column">
-    <div class="card">
-      <img src="/w3images/team2.jpg" alt="Mike" style="width:100%">
-      <div class="container">
-        <h2>Mike Ross</h2>
-        <p class="title">Art Director</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>mike@example.com</p>
-        <p><button class="button">Contact</button></p>
-      </div>
+        <div class="column">
+            <div class="card">
+                <img src="/w3images/team2.jpg" alt="Mike" style="width:100%">
+                <div class="container">
+                    <h2>Mike Ross</h2>
+                    <p class="title">Art Director</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>mike@example.com</p>
+                    <p><button class="button">Contact</button></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="/w3images/team3.jpg" alt="John" style="width:100%">
+                <div class="container">
+                    <h2>John Doe</h2>
+                    <p class="title">Designer</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>john@example.com</p>
+                    <p><button class="button">Contact</button></p>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-  
-  <div class="column">
-    <div class="card">
-      <img src="/w3images/team3.jpg" alt="John" style="width:100%">
-      <div class="container">
-        <h2>John Doe</h2>
-        <p class="title">Designer</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>john@example.com</p>
-        <p><button class="button">Contact</button></p>
-      </div>
-    </div>
-  </div>
-</div>
 
 </body>
+
 </html>
